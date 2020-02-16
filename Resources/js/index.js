ec1f7519afb5b71a5e353ac397263a3f9b2578eb
Zhengze Gong <zhengzeg@andrew.cmu.edu>
2020-02-15 21:42:48 -0500

    added background img; improved color scheme; added about section; added version info in footer; separated js code into its own file; added global variables in index.css

---
+++ Resources/js/index.js
@@ -0,0 +1,52 @@
+// play sound
+let hurtSound = document.getElementById('hurt');
+function playAudio() {
+  hurtSound.play();
+}
+function search(){
+    let userInput = document.getElementById('keyword').value;
+    window.open('https://minecraft.gamepedia.com/' + userInput);
+}
+
+// dropdown menu controller
+let open = false;
+function drp_handler() {
+  let image = document.getElementById('drp_menu');
+  let menu = document.getElementById('header');
+  if (open == true) {
+    image.src = './Resources/img/chest_closed.png';
+    header.style.transition = '0.15s ease-out';
+    menu.style.height = '74px';
+    open = false;
+    setTimeout(function stop_animation() {
+      header.style.transition = 'none';
+    }, 150);
+  }
+  else {
+    image.src = './Resources/img/chest_open.png';
+    header.style.transition = '0.15s ease-out';
+    menu.style.height = '205px';
+    open = true;
+    setTimeout(function stop_animation() {
+      header.style.transition = 'none';
+    }, 150);
+
+  }
+}
+
+const width = window.matchMedia('(min-width: 800px)');
+function drp_collaspe(width) {
+  let image = document.getElementById('drp_menu');
+  let menu = document.getElementById('header');
+  if (width.matches) { // if width >= 800px
+    image.src = './Resources/img/chest_closed.png';
+    menu.style.height = '74px';
+    open = false;
+  }
+}
+
+width.addListener(drp_collaspe);
+drp_collaspe(width);
+
+let heart = getElementById('heart');
+let developerText = getElementById('footer-developer');
