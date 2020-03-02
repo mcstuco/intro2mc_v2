48ce5f39dc139438adf8e1f1b5fbf8170013ed74
Zhengze Gong <zhengzeg@andrew.cmu.edu>
2020-03-01 21:51:52 -0500

    added style for resources: added icons, added some new links; added portals section; completed some links

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,52 +1,98 @@
-// play sound
-let hurtSound = document.getElementById('hurt');
-function playAudio() {
-  hurtSound.play();
-}
-function search(){
-    let userInput = document.getElementById('keyword').value;
-    window.open('https://minecraft.gamepedia.com/' + userInput);
-}
+function changeVideoHeight(method) {
+  let COL_COUNT_DEFAULT = 2;
+  let root = document.documentElement;
+  let videos = document.getElementById("videos");
+  let count = videos.childElementCount;
+  let rowCount = getComputedStyle(document.documentElement).getPropertyValue('--videos-number-per-row');
+  let showAllButton = document.getElementById('video-show-all-button');
+  let showLessButton = document.getElementById('video-show-less-button');
 
-// dropdown menu controller
-let open = false;
-function drp_handler() {
-  let image = document.getElementById('drp_menu');
-  let menu = document.getElementById('header');
-  if (open == true) {
-    image.src = './Resources/img/chest_closed.png';
-    header.style.transition = '0.15s ease-out';
-    menu.style.height = '74px';
-    open = false;
-    setTimeout(function stop_animation() {
-      header.style.transition = 'none';
-    }, 150);
+  videos.style.transition = '0.15s ease-out';
+  setTimeout(function stop_animation() {
+    videos.style.transition = 'none';
+  }, 150);
+
+  if (method == 'all') {
+    root.style.setProperty('--videos-number-per-col', Math.ceil(count / rowCount));
+    showAllButton.style.display = 'none';
+    showLessButton.style.display = 'block';
   }
-  else {
-    image.src = './Resources/img/chest_open.png';
-    header.style.transition = '0.15s ease-out';
-    menu.style.height = '205px';
-    open = true;
-    setTimeout(function stop_animation() {
-      header.style.transition = 'none';
-    }, 150);
-
+  else if (method == 'less') {
+    root.style.setProperty('--videos-number-per-col', COL_COUNT_DEFAULT);
+    showLessButton.style.display = 'none';
+    showAllButton.style.display = 'block';
   }
 }
 
-const width = window.matchMedia('(min-width: 800px)');
-function drp_collaspe(width) {
-  let image = document.getElementById('drp_menu');
-  let menu = document.getElementById('header');
-  if (width.matches) { // if width >= 800px
-    image.src = './Resources/img/chest_closed.png';
-    menu.style.height = '74px';
-    open = false;
+function randomTexts() {
+  let randomNum = Math.floor(Math.random() * 10);
+  let heart = document.getElementById('heart');
+  let developerText = document.getElementById('is-text');
+
+  switch (randomNum) {
+    case 1:
+      heart.src = './Resources/img/hearts/empty_heart.svg';
+      break;
+    case 2:
+      heart.src = './Resources/img/hearts/half_heart.svg';
+      break;
+    case 3:
+      heart.src = './Resources/img/hearts/hardcore_heart.svg';
+      break;
+    case 4:
+      heart.src = './Resources/img/hearts/poisoned_heart.svg';
+      break;
+    case 5:
+      heart.src = './Resources/img/hearts/withered_heart.svg';
+      break;
+    default:
+      heart.src = './Resources/img/hearts/heart.svg';
+  }
+
+  randomNum = Math.floor(Math.random() * 10);
+  switch (randomNum) {
+    case 1:
+      developerText.innerHTML = 'is (probably)';
+      break;
+    case 2:
+      developerText.innerHTML = 'must be';
+      break;
+    case 3:
+      developerText.innerHTML = 'should be';
+      break;
+    case 4:
+      developerText.innerHTML = 'could be';
+      break;
+    case 5:
+      developerText.innerHTML = 'is definitely';
+      break;
+    case 6:
+      developerText.innerHTML = 'is always';
+      break;
+    default:
+      developerText.innerHTML = 'is';
   }
 }
 
-width.addListener(drp_collaspe);
-drp_collaspe(width);
+function search(url){
+    var userInput = document.getElementById("keyword").value;
+    window.open(url + userInput);
+}
 
-let heart = getElementById('heart');
-let developerText = getElementById('footer-developer');
+function unfinishedFeature() {
+  alert("This feature is still under development :)");
+}
+
+function main() {
+  randomTexts();
+
+  let videos = document.getElementById("videos");
+  let count = videos.childElementCount;
+  let showAllButton = document.getElementById('video-show-all-button');
+  let rowCount = getComputedStyle(document.documentElement).getPropertyValue('--videos-number-per-row');
+  let colCount = getComputedStyle(document.documentElement).getPropertyValue('--videos-number-per-col');
+
+  if (count <= rowCount * colCount) {
+    showAllButton.style.display = 'none';
+  }
+}
---
+++ Resources/js/resources.js
@@ -0,0 +1,4 @@
+function search(){
+    let userInput = document.getElementById('keyword').value;
+    window.open('https://minecraft.gamepedia.com/' + userInput);
+}
